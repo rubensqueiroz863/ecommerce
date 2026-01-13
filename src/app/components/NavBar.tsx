9199d4ef55598740f32e64364c645b85095cd665
rubensqueiroz863 <rubensqueiroz863@exemplo.com>
2026-01-13 13:46:40 -0300

    feat(navbar): NavBar criada e estilizada

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar(){
+  return (
+    <div className="flex items-center justify-between xl:px-20 px-10 bg-(--bg-main) h-12">
+
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import NavBar from "./components/NavBar";
+
 export default function HomePage() {
   return (
     <div>
-      
+      <NavBar />
+      <div className="w-full h-px bg-(--hover-border)"></div>
     </div>
   );
 }

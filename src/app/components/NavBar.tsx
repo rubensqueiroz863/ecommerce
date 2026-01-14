a6560df50ee7c7b8e219e19a127448948d1b5110
rubensqueiroz863 <rubensqueiroz863@exemplo.com>
2026-01-14 17:43:35 -0300

    feat(menu-button): Menu button criado e instalação da biblioteca framer-motion

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import Logo from "./Logo";
+import Menu from "./Menu";
 
 export default function NavBar(){
   return (
     <div className="flex items-center justify-between xl:px-20 px-10 bg-(--bg-main) h-12">
       <Logo />
+      <Menu />
     </div>
   );
 }

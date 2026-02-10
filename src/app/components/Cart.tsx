ee53e54002eacd103b52c5fc34156e68244d1b15
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2026-02-10 19:58:20 -0300

    fix(cartPng): Ajuste no menu do cart para mudança de cir dependendo do tema

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,20 +1,34 @@
+"use client";
+
 import { useCart } from "@/lib/cart";
+import { useTheme } from "next-themes";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const cart = useCart();
   return (
     <button
       onClick={() => cart.toggleCart()}
       className="cursor-pointer"
     >
-      <Image
-        src={"https://i.postimg.cc/VN22vfk0/imag32es-removebg-preview.png"}
-        width={128}
-        height={128}
-        alt="Cart button"
-        className="w-6 h-6"
-      />
+      {mounted && (
+        <Image
+          src={theme === "dark" ? "https://i.postimg.cc/VN22vfk0/imag32es-removebg-preview.png" : "https://i.postimg.cc/zfbSJCPG/25619.png"}
+          width={128}
+          height={128}
+          alt="Cart button"
+          className="w-6 h-6"
+        />
+      )}
+      
     </button>
   );
 }
